1534ed0e3e983210cbc7aeb7cc5e361d192b9279
신준혁 <oridori2705@naver.com>
2023-12-03 03:07:09 +0900

    commit: 영화 검색 API 호출 구현

---
+++ src/components/MovieSearchBar.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+import TheIcon from '~/components/TheIcon.vue'
+import { ref } from 'vue'
+import { useMovieStore } from '~/store/movies'
+const movieName = ref('')
+
+const movieStore = useMovieStore()
+
+async function searchMovie(event: MouseEvent | KeyboardEvent) {
+  //한글 입력시 브라우저에서 구조화하는 것 때문에 두번 요청이 들어감
+  if (event instanceof KeyboardEvent && event.isComposing) return
+  if (!movieName.value.trim()) return
+  try {
+    await movieStore.fetchMovies({
+      movieName: movieName.value
+    })
+    movieName.value = ''
+  } catch (error) {
+    console.error('TodoCreator/createTodo failed')
+  }
+}
+</script>
+
+<template>
+  <div class="moive-search">
+    <input
+      placeholder="영화를 검색해보세요!"
+      :value="movieName"
+      @input="movieName = ($event.target as HTMLInputElement).value"
+      @keydown.enter="searchMovie" />
+    <TheIcon>search</TheIcon>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.moive-search {
+  height: 80px;
+  display: flex;
+  position: relative;
+  :deep(.the-icon) {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    margin: auto;
+    left: 24px;
+    z-index: 1;
+  }
+  input {
+    padding: 0 10px 0 80px;
+    border: none;
+    outline: none;
+    border-radius: 6px;
+    background-color: #fff;
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    &::placeholder {
+      color: #ccc;
+    }
+  }
+}
+</style>
+~/store/movies
--- src/store/movies.ts
+++ src/store/movies.ts
@@ -1,7 +1,38 @@
 import { defineStore } from 'pinia'
+import { request } from '~/api/api'
 
-export const useTodosStore = defineStore('todos', {
-  state: () => ({}),
+export interface ResponseValue {
+  Search: Search[]
+  totalResults: string
+  Response: string
+}
+export type Search = {
+  Title: string
+  Year: string
+  imdbID: string
+  Type: string
+  Poster: string
+}
+
+interface searchMoviePayload {
+  movieName: string
+  page?: number
+}
+
+export const useMovieStore = defineStore('moives', {
+  state: () => ({
+    movies: {} as ResponseValue
+  }),
   getters: {},
-  actions: {}
+  actions: {
+    async fetchMovies({ movieName, page = 1 }: searchMoviePayload) {
+      try {
+        const searchMovies = await request({ movieName, page })
+
+        this.movies = searchMovies
+      } catch (error) {
+        console.error('fetchedTodos failed', error)
+      }
+    }
+  }
 })
