a37c36327bb9e5abee78715f152ab99f34bbaa20
신준혁 <oridori2705@naver.com>
2023-12-03 00:50:01 +0900

    commit: google Icon 사용을 위한 컴포넌트 구현

---
+++ src/components/TheIcon.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="the-icon">
+    <span class="material-symbols-outlined"> <slot></slot> </span>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.the-icon {
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+}
+</style>
