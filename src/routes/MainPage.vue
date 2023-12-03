4fc0f14c48aadf1f22a5945dd1cda57e1f21cbc1
신준혁 <oridori2705@naver.com>
2023-12-03 15:17:29 +0900

    commit: 검색어 입력 유도문 추가

--- src/routes/MainPage.vue
+++ src/routes/MainPage.vue
@@ -1,18 +1,30 @@
 <script setup lang="ts">
 import MovieSearchBar from '~/components/MovieSearchBar.vue'
 import MovieSearchResult from '~/components/MovieSearchResult.vue'
+import { useMovieStore } from '~/store/movies'
+
+const movieStore = useMovieStore()
+console.log(movieStore.movies)
 </script>
 
 <template>
   <main>
     <MovieSearchBar />
-    <MovieSearchResult />
+    <MovieSearchResult v-if="Object.keys(movieStore.movies).length" />
+    <div
+      v-else
+      class="search-text">
+      <p>검색어를 입력하세요!</p>
+    </div>
   </main>
 </template>
 
-<style scoped lang="scss">
-.main {
-  max-width: 700px;
+<style lang="scss" scoped>
+main {
+  max-width: 1300px;
   margin: 0 auto;
+  .search-text {
+    margin-top: 50px;
+  }
 }
 </style>
