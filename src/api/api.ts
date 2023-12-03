88a47eb70ef9705e89842aa340992b23a6150f30
신준혁 <oridori2705@naver.com>
2023-12-03 15:09:28 +0900

    fix: console.log 제거

--- src/api/api.ts
+++ src/api/api.ts
@@ -13,7 +13,7 @@ interface RequestBody {
 
 export const request = async (req: Partial<RequestBody>) => {
   const { movieName = '', page = 1, method = 'GET', id = '', plot = '' } = req
-  console.log(movieName, page)
+
   try {
     const { data: responseValue } = await axios({
       url: `${API_END_POINT}?apikey=${API_KEY}&${
